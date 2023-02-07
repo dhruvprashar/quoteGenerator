1cf3ae533bf6cc2e09406d7e12fd8b5082c73cc7
Dhruv <prashardhruv2229@gmail.com>
2023-02-07 23:08:35 +0530

    loader added

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ const quoteText= document.getElementById('quote');
 const authorText= document.getElementById('author');
 const twitterBtn= document.getElementById('twitter');
 const newQuoteBtn= document.getElementById('new-quote');
+const loader= document.getElementById('loader');
+
+
 // Get Quotes from API
 let apiQuotes=[];
+
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden=true;
+}
+function complete(){
+    loader.hidden = true;
+    quoteContainer.hidden=false;
+}
+
 function newQuote(){
+    loading();
     //Pick random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
     // console.log(quote);
@@ -21,10 +35,14 @@ function newQuote(){
     } else{
         quoteText.classList.remove('long-quote');
     }
+    //set quote and Hide loader
+
     quoteText.textContent=quote.text;
+    complete();
 }
 
 async function getQuotes(){
+    loading();
     const apiUrl='https://type.fit/api/quotes'
     try {
         const response= await fetch(apiUrl);
@@ -54,4 +72,5 @@ twitterBtn.addEventListener('click',tweetQuote);
 
 //On load
 getQuotes();
+// loading();
 
